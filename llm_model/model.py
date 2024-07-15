9bd444088a2440c19d8a492783d5e8ea1677fbe1
Siddarth <pullakhandam.siddartha@gmail.com>
2024-07-14 23:50:16 -0400

    added .env_example file

--- llm_model/model.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-load_dotenv()
-from langchain_openai import AzureChatOpenAI, ChatOpenAI
-model = AzureChatOpenAI(openai_api_version=os.environ.get("AZURE_OPENAI_VERSION", "2023-07-01-preview"),
-    azure_deployment=os.environ.get("AZURE_OPENAI_DEPLOYMENT", "gpt4chat"),
-    azure_endpoint=os.environ.get("AZURE_OPENAI_ENDPOINT", "https://gpt-4-trails.openai.azure.com/"),
-    api_key=os.environ.get("AZURE_OPENAI_KEY"),
-    temperature=0.3)
